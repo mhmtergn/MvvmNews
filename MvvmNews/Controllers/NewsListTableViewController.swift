196f101987f1a681711006e077ffb33ef643bc58
Mehmet Ergun <mrmehmetergun@gmail.com>
2022-06-22 21:48:03 +0300

    first commit

---
+++ MvvmNews/Controllers/NewsListTableViewController.swift
@@ -0,0 +1,8 @@
+//
+//  NewsListTableViewController.swift
+//  MvvmNews
+//
+//  Created by Mehmet Ergün on 2022-06-21.
+//
+
+import Foundation
---
+++ MvvmNews/ViewModels/ArticleViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ArticleViewModel.swift
+//  MvvmNews
+//
+//  Created by Mehmet Ergün on 2022-06-22.
+//
+
+import Foundation
